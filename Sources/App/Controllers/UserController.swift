4ee5047a7bfc7575dd075ad7b182c41b34539324
sunaryaagung95 <sunaryaagung95@gmail.com>
2019-12-19 20:49:09 +0700

    add user model, controller & router

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -9,6 +9,12 @@ final class UserController: RouteCollection {
         users.post(User.self, use: create)
 
         users.get(use: getAll)
+        users.get(User.parameter, use: getOne)
+        users.get("search", use: search)
+
+        users.put(UserContent.self, at: User.parameter, use: update)
+
+        users.delete(User.parameter, use: delete)
     }
 
     func create(_ req: Request, _ user: User) throws -> Future<User> {
@@ -20,5 +26,33 @@ final class UserController: RouteCollection {
         return User.query(on: req).all()
     }
 
+    func getOne(_ req: Request) throws -> Future<User> {
+        return try req.parameters.next(User.self)
+    }
 
+    func search(_ req: Request) throws -> Future<[User]> {
+        let email = try req.query.get(String.self, at: "email")
+        return User.query(on: req).group(.or) { query in
+            query.filter(\.email =~ email)
+        }.all()
+    }
+
+    func update(_ req: Request, _ body: UserContent) throws -> Future<User> {
+        let user = try req.parameters.next(User.self)
+        return user.map(to: User.self) { user in
+            user.email = body.email ?? user.email
+            user.password = body.password ?? user.password
+            user.password = try BCrypt.hash(user.password)
+            return user
+        }.update(on: req)
+    }
+
+    func delete(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(User.self).delete(on: req).transform(to: .noContent)
+    }
+}
+
+struct UserContent: Content {
+    var email: String?
+    var password: String?
 }
