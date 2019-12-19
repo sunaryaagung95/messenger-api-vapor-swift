541a56520011b9eea2bd1d3f269371954d86b8a6
sunaryaagung95 <sunaryaagung95@gmail.com>
2019-12-19 23:48:51 +0700

    add message controller

---
+++ Sources/App/Controllers/MessageController.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Vapor
+import Fluent
+
+final class MessageController: RouteCollection {
+    func boot(router: Router) throws {
+        let messages = router.grouped("api", "messages")
+
+        messages.post(Message.self, use: create)
+        
+        messages.get(use: getAll)
+
+        messages.delete(Message.parameter, use: delete)
+    }
+
+    func create(_ req: Request, _ message: Message) throws -> Future<Message> {
+        return message.save(on: req)
+    }
+
+    func getAll(_ req: Request) throws -> Future<[Message]> {
+        return Message.query(on: req).all()
+    }
+
+    func delete(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Message.self).delete(on: req).transform(to: .noContent)
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,5 @@ public func routes(_ router: Router) throws {
     }
 
    try router.register(collection: UserController())
+   try router.register(collection: MessageController())
 }
